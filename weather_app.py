77c755a48eb95d3974fe4970db7aa4ea01e9a64d
Aaron Wilkinson <aaronwilkinson@gmail.com>
2018-04-10 18:44:41 +0000

    functionality increase

--- weather_app.py
+++ weather_app.py
@@ -6,8 +6,7 @@ import tornado.web
 import json
 import requests
 
-from jinja2 import \
-    Environment, PackageLoader, select_autoescape
+from jinja2 import Environment, PackageLoader, select_autoescape
   
 ENV = Environment(
     loader=PackageLoader('app', 'templates'),
@@ -21,9 +20,37 @@ class TemplateHandler(tornado.web.RequestHandler):
 
 class MainHandler(TemplateHandler):
     def get(self):
+        units = self.get_query_argument("units", None)
         user_input = self.get_query_argument("user_input", None)
         if user_input:
-            self.render_template("results.html", {})
+            try:
+                # zipcode entered
+                int(user_input)
+                current_weather = requests.get('http://api.openweathermap.org/data/2.5/weather?zip={}&APPID=729e8479b0893cb6745505ba71830535'.format(user_input))
+            except ValueError:
+                # city name entered
+                current_weather = requests.get('http://api.openweathermap.org/data/2.5/weather?q={}&APPID=729e8479b0893cb6745505ba71830535'.format(user_input))
+            
+            current_weather = current_weather.json()
+            
+            try:
+                current_temp = current_weather['main']['temp']
+            except KeyError:
+                # city name does not exist
+                self.render_template("home.html", {"message": "City not found"})
+                return
+                
+            conditions = current_weather['weather']
+            
+            if units == "metric":
+                current_temp -= 273.15
+            elif units == "imperial":
+                current_temp = current_temp * 9/5 - 459.67
+            else:
+                pass
+            
+            current_temp = "%.1f" % current_temp
+            self.render_template("results.html", {"current_temp": current_temp, "units": units, "conditions": conditions})
         else:
             self.render_template("home.html", {})
         
